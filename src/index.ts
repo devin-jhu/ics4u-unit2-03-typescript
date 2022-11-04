8b0632718f37cffa3e4e482c366cd46fe57e27d7
devin jhu <devin.jhu@mths.ca>
2022-11-04 00:36:42 +0000

    code

---
+++ src/index.ts
@@ -0,0 +1,38 @@
+/**
+ * the stack calling program
+ *
+ * By:      Devin Jhu
+ * Version: 1.0
+ * Since:   2022-10-31
+ */
+
+import MrCoxallStack from './MrCoxallStack'
+import promptSync from 'prompt-sync'
+
+const prompt = promptSync()
+const myStack = new MrCoxallStack()
+
+// Input & Process
+while (true) {
+  const userNumber = prompt('Enter number(type . to end loop): ')
+
+  if (userNumber === '.') {
+    break
+  } else {
+    const number = parseInt(userNumber)
+    if (isNaN(number)) {
+      console.log(`${userNumber} is not a number`)
+    } else {
+      myStack.pushed(number)
+    }
+  }
+}
+const tempStack = myStack.getStack()
+console.log(tempStack)
+
+console.log(myStack.getStack())
+const popValue = myStack.popNumber()
+console.log('\nPopped Item:')
+console.log(popValue)
+
+console.log('\nDone.')
